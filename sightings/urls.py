57aa955ba105045aa31aff809151b3716d7eeecd
Caroline ling <zl2843@columbia.edu>
2019-12-06 06:24:22 +0000

    Modify urls

--- sightings/urls.py
+++ sightings/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
         path('', views.all_squirrels),
         path('add/', views.add_squirrel),
         path('edit/', views.edit_squirrel),
-        path('<Unique_Squirrel_ID>/', views.details),
         ]
 
